289198d8b045555f6f9f694d711fca88b6e89749
khinyeemonthant <kymthant@gmail.com>
2022-12-16 08:21:23 +0630

    review fixed

--- Tutorial_04/index.php
+++ Tutorial_04/index.php
@@ -11,34 +11,49 @@ if (isset($_SESSION["name"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/reset.css">
-    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Log in & Log out</title>
 </head>
 
 <body>
     <?php
-    $name = $nameErr = $pwd = $pwdErr = "";
+    define("USER", "username");
+    define("PASSWORD", "123456");
+    $name = $nameErr = $pwd = $pwdErr = $invalid = "";
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (empty($_POST["name"])) {
             $nameErr = "*Name is required";
+        } else if ($_POST["name"] != USER) {
+            $nameErr =  "Invalid username!";
         } else {
             $name = $_POST["name"];
         }
+
         if (empty($_POST["pwd"])) {
             $pwdErr = "*Password is required";
+        } else if ($_POST['pwd'] != PASSWORD) {
+            $pwdErr = "Invalid password!";
         }
-        if (!empty($_POST["name"]) && !empty($_POST["pwd"])) {
+
+        if ($_POST["name"] == USER && $_POST["pwd"] == PASSWORD) {
             $_SESSION["name"] = $_POST["name"];
+            $_SESSION["pwd"] = $_POST["pwd"];
             header("location:home.php");
         }
     }
+
     ?>
+    <div class="error-container">
+        <span class="invalid"><?php echo $invalid; ?></span>
+    </div>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="login-form">
-        <input type="text" name="name" placeholder="Enter Username" value="<?php echo $name; ?>"><br>
-        <span class="error"><?php echo $nameErr; ?></span><br>
-        <input type="password" name="pwd" placeholder="Enter Password" value=""><br>
-        <span class="error"><?php echo $pwdErr; ?></span><br>
+        <input type="text" name="name" placeholder="Enter Username" value="<?php echo $name; ?>">
+        <span class="error"><?php echo $nameErr; ?></span>
+        <input type="password" name="pwd" placeholder="Enter Password" value="">
+        <span class="error">
+            <?php echo $pwdErr; ?>
+        </span>
         <button type="submit" class="btn">Login</button>
     </form>
 </body>
--- Tutorial_06/index.php
+++ Tutorial_06/index.php
@@ -11,77 +11,98 @@
 </head>
 
 <body>
-    <div class="upload">
-        <form action="" method="POST" enctype="multipart/form-data">
-            <label for="folder">Enter Folder Name</label>
-            <input type="text" name="folder" value=""><br>
-            <input type="file" name="img"><br>
-            <input type="submit" name="submit" value="Upload">
-        </form>
-    </div>
     <?php
-    $dirs = array_filter(glob('*'), 'is_dir');
-    foreach ($dirs as $dir) {
-        $images = glob($dir . DIRECTORY_SEPARATOR . "*.{jpg,png,gif}", GLOB_BRACE);
-        foreach ($images as $image) { ?>
-            <form action="" method="POST" class="container">
-                <input value="<?php echo $image; ?>" name="file" type="hidden">
-                <div class="thumbnail"><img src="<?php echo $image; ?>" name="del-img"></div>
-                <input type="submit" value="Delete" name="del-btn" class="del-btn">
-            </form>
-            <?php }
-    }
-    if (isset($_POST['del-btn'])) {
-        unlink($_POST['file']);
-    }
+    $requireFolder = $requireImage = $message = $folder = $error = "";
     if (isset($_POST['submit'])) {
+
         if (empty($_POST['folder'])) {
-            echo "folder name require";
-        }
-        if ($_FILES['img']['size'] == 0) {
-            echo "image file require";
-        }
-        if (!empty($_POST['folder']) && $_FILES['img']['size'] != 0) {
+            $requireFolder = "*Folder name require";
+        } else {
             $folder = $_POST['folder'];
+        }
+
+        if ($_FILES['img']['size'] == 0) {
+            $requireImage = "*Image file require";
+        }
+
+        if (!empty($_POST['folder']) && $_FILES['img']['size'] != 0) {
+            $uploadOk = 1;
             $img_name = $_FILES['img']['name'];
             $img_tmpname = $_FILES['img']['tmp_name'];
             $img_size = $_FILES['img']['size'];
+
             //Check if folder already exists
             if (!is_dir($folder)) {
                 mkdir($folder);
             }
-            $uploadOk = 1;
+
+            // Check if file already exists
             $path = $folder . DIRECTORY_SEPARATOR;
             $dir = $path . $img_name;
-            // Check if file already exists
             if (file_exists($dir)) {
-                echo "File already exists.";
+                $error = "File already exists.";
                 $uploadOk = 0;
             }
+
+            //Allow only JPG, JPEG, PNG & GIF etc formats
             $allowed_types = array('jpg', 'png', 'jpeg', 'gif');
             $file_ext = strtolower(pathinfo($dir, PATHINFO_EXTENSION));
-            //Allow only JPG, JPEG, PNG & GIF etc formats
             if ($file_ext != "jpg" && $file_ext != "png" && $file_ext != "jpeg" && $file_ext != "gif") {
-                echo "Only JPG, JPEG, PNG & GIF files are allowed";
+                $error = "Only JPG, JPEG, PNG & GIF files are allowed";
                 $uploadOk = 0;
-            }
             // Check file size
-            if ($img_size > 500000) {
-                echo "File is too large.";
+            } elseif ($img_size > 500000) {
+                $error = "File is too large.";
                 $uploadOk = 0;
             }
-            if ($uploadOk == 0) {
-                echo "Sorry, your file was not uploaded.";
-            } else {
-                if (move_uploaded_file($img_tmpname, $dir)) {
-                    echo "The file " . $img_name . " has been uploaded.";
-                    } else {
-                    echo "Sorry, there was an error uploading your file.";
-                }
+
+            //Upload
+            if ($uploadOk == 1) {
+                move_uploaded_file($img_tmpname, $dir);
+                $message = "The file " . $img_name . " has been uploaded.";
+                $folder = "";
             }
         }
     }
+
+    if (isset($_GET['delete'])) {
+        unlink($_GET['delete']);
+        header("location:index.php");
+    }
     ?>
+    <form action="" method="POST" enctype="multipart/form-data" class="upload">
+        <label for="folder">Enter Folder Name</label>
+        <input type="text" name="folder" value="<?php echo $folder; ?>">
+        <span class="require"><?php echo $requireFolder ?></span>
+        <input type="file" name="img" class="input-box">
+        <span class="require"><?php echo $requireImage ?></span>
+        <span class="error"><?php echo $error ?></span>
+        <button type="submit" name="submit" class="submit">Upload</button>
+    </form>
+    <div class="message">
+        <p><?php echo $message; ?></p>
+    </div>
+    <table class="img-table">
+        <tr>
+            <th>Image</th>
+            <th>Directory</th>
+            <th>Action</th>
+        </tr>
+        <?php
+        $dirs = array_filter(glob('*'), 'is_dir');
+        foreach ($dirs as $dir) {
+            $images = glob($dir . DIRECTORY_SEPARATOR . "*.{jpg,png,gif}", GLOB_BRACE);
+            foreach ($images as $image) { ?>
+                <tr>
+                    <td><img src="<?php echo $image; ?>"></td>
+                    <td>
+                        <p><?php echo $image; ?></p>
+                    </td>
+                    <td><a href="?delete=<?php echo $image; ?>" class="del-btn">Delete</a></td>
+                </tr>
+        <?php }
+        } ?>
+        <table>
 </body>
 
 </html>
